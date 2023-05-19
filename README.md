99193564888aab4a18a78388bd5896fa1f88a18a
AriadnaHCTec <A01749161@tec.mx>
2023-05-19 13:49:11 -0600

    Ariadna simple drive

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# simple_drive
+# simple_drive [![Build Status](http://build.ros.org/buildStatus/icon?job=Kbin_uX64__simple_drive__ubuntu_xenial_amd64__binary)](http://build.ros.org/job/Kbin_uX64__simple_drive__ubuntu_xenial_amd64__binary)
 
-## Description
+A simple robot drive system that includes skid steering joystick teleoperation, control of a panning servo to look around the robot, and Arduino firmware.
 
-A simple robot drive system that includes skid steering joystick teleoperation, control of a panning two servos to look around the robot, and Arduino firmware.
-This node only publishes the linear and angular velocity. The Closed Loop Velocity Control is made by this other [node](https://github.com/QuantumRoboticsURC/bus_can_drive).
-
+![controller](http://wiki.ros.org/simple_drive?action=AttachFile&do=get&target=Xbox_Controller.png)
 ##
-![controller](https://raw.githubusercontent.com/QuantumRoboticsURC/simple_drive/main/images/simple_drive_controller.png)
+![simple_drive](http://wiki.ros.org/simple_drive?action=AttachFile&do=get&target=Simple_Drive_Diagram.png)
 
 ## Quick Start
 
 1. Install:
 
 ```
-$ cd catkin/ws
-$ git clone https://github.com/QuantumRoboticsURC/simple_drive.git
+$ sudo apt-get install ros-kinetic-simple-drive
 ```
 
 2. Launch ROS nodes:
 
 ```
 $ roslaunch simple_drive drive_teleop.launch joy_dev:=/dev/input/js0
+$ roslaunch simple_drive cmd_vel_mux.launch
+$ roslaunch simple_drive simple_drive.launch serial_dev:=/dev/ttyACM0
+
+OR all-in-one launch:
+$ roslaunch simple_drive drive.launch
 ```
+
+3. Install the drive_firmware onto a microcontroller connected to motors and wheels.
+
+4. Drive your robot around.
+
+**Full documentation on wiki: [http://wiki.ros.org/simple_drive](http://wiki.ros.org/simple_drive)**
--- src/simple_drive/cmd_vel_mux.py
+++ src/simple_drive/cmd_vel_mux.py
@@ -3,50 +3,28 @@
 import rospy
 import time
 
-from std_msgs.msg import *
 from geometry_msgs.msg import Twist
-#variable de decision
-objeto = 0
 
-def detectobjeto(flag):
-    global objeto
-    objeto = int(flag.data)
-    pub2.publish(str(type(objeto)))
+class CmdVelMux:
+    def __init__(self):
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
+        rospy.Subscriber("move_base/cmd_vel", Twist, callback=self.on_autonomous_cmd)
+        rospy.Subscriber("teleop/cmd_vel", Twist, callback=self.on_human_cmd)
+        self.block_duration = 0
+        self.human_cmd_time = time.time()
 
-def on_autonomous_cmd(twist):
-    time_since_human_cmd = time.time() - human_cmd_time
-    if time_since_human_cmd >= block_duration:
-        block_duration = 0 # stop blocking
-        pub.publish(twist)
+    def on_autonomous_cmd(self, twist):
+        time_since_human_cmd = time.time() - self.human_cmd_time
+        if time_since_human_cmd >= self.block_duration:
+            self.block_duration = 0 # stop blocking
+            self.pub.publish(twist)
 
-def on_human_cmd(twist): #Esta funcion transforma el decision auto
-    global objeto
-    human_cmd_time = time.time()
-    block_duration = rospy.get_param('~block_duration', 5) # to get the lowest latency, comment this or hard code this so that there is no call to rospy.get_param
-    #pub.publish(twist)
-    #TOMA LA DECISION DE OBEDECER O PARAR        
-    if(objeto == 0):
-        pub.publish(twist)
-        mtri.publish(0)
-    elif(objeto == 1):
-        twist0 = Twist()
-        twist0.linear.x = 0
-        twist0.angular.z = 0
-        pub.publish(twist0)
-        mtri.publish(1)
+    def on_human_cmd(self, twist):
+        self.human_cmd_time = time.time()
+        self.block_duration = rospy.get_param('~block_duration', 5) # to get the lowest latency, comment this or hard code this so that there is no call to rospy.get_param
+        self.pub.publish(twist)
 
-
-pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-pub2 = rospy.Publisher("val", String, queue_size=10)
-mtri = rospy.Publisher('/matrix',Int64,queue_size = 10)
 def main():
     rospy.init_node("cmd_vel_mux")
-    
-    ## TOPICO QUE AYUDA A SABER SI HAY DETECCION Y EN CASO AFIRMATIVO, CAMBIAR LA VARIABLE GLOBAL
-    rospy.Subscriber("detection",String, detectobjeto)
-    #rospy.Subscriber("move_base/cmd_vel", Twist, on_autonomous_cmd)
-    rospy.Subscriber("teleop/cmd_vel", Twist, on_human_cmd)
-    block_duration = 0
-    human_cmd_time = time.time()
+    muxer = CmdVelMux()
     rospy.spin()
-
